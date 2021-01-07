2e8cf83a5e927fb94e127b1cded07ce79aac4d68
Evan Spangenberg <ewlson23@gmail.com>
2021-01-07 15:52:39 -0500

    initialized int array and int variables

--- src/main/java/LongestIncreasingSubsequence/FindLongestIncreasingSubsequence.java
+++ src/main/java/LongestIncreasingSubsequence/FindLongestIncreasingSubsequence.java
@@ -2,41 +2,11 @@ package LongestIncreasingSubsequence;
 
 public class FindLongestIncreasingSubsequence {
 
-	static int max_ref;
-
 	static int longestIncreasingSubsequence(int arr[], int n) {
+		int lis[] = new int[n];
+		int i, j, max = 0;
+		
+		return 0;
 
-		if (n == 1)
-			return 1;
-
-		int res, max_ending_here = 1;
-
-		for (int i = 1; i < n; i++) {
-			res = longestIncreasingSubsequence(arr, i);
-
-			if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here)
-				max_ending_here = res + 1;
-		}
-
-		if (max_ref < max_ending_here)
-			max_ref = max_ending_here;
-
-		return max_ending_here;
 	}
-
-	static int lisWrapper(int arr[], int n) {
-
-		max_ref = 1;
-
-		longestIncreasingSubsequence(arr, n);
-
-		return max_ref;
-	}
-
-	public static void main(String args[]) {
-		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
-		int n = arr.length;
-		System.out.println("Length of longest increasing subsequence is " + lisWrapper(arr, n) + "\n");
-	}
-
 }
