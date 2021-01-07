44e3ab191ecfde4c7a92f8eeefac0dce9da7a80f
Evan Spangenberg <ewlson23@gmail.com>
2021-01-07 15:42:07 -0500

    added recursive iteration through int array method

--- src/main/java/LongestIncreasingSubsequence/FindLongestIncreasingSubsequence.java
+++ src/main/java/LongestIncreasingSubsequence/FindLongestIncreasingSubsequence.java
@@ -1,10 +1,27 @@
 package LongestIncreasingSubsequence;
 
 public class FindLongestIncreasingSubsequence {
-	
-	
-	public static void main(String [] args) {
-		
-	}
+
+	static int max_ref;
+
+	static int longestIncreasingSubsequence(int arr[], int n) {
+
+		if (n == 1)
+			return 1;
+
+		int res, max_ending_here = 1;
+
+		for (int i = 1; i < n; i++) {
+			res = longestIncreasingSubsequence(arr, i);
+
+			if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here)
+				max_ending_here = res + 1;
+		}
+
+		if (max_ref < max_ending_here)
+			max_ref = max_ending_here;
+
+		return max_ending_here;
+
 
 }
